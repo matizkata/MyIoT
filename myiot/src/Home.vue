68728d049b126f77be55cf2698a7facc1645c306
Zerven1 <160744@stud.prz.edu.pl>
2020-08-13 16:29:23 +0200

    naprawa problemów

--- myiot/src/Home.vue
+++ myiot/src/Home.vue
@@ -20,21 +20,7 @@ export default {
 
 </style>
 
-<div>
-  <Homep/>
-</div>
-</template>
 
 
-<script>
 
-import Homep from './components/Homep';
-export default {
-  name: 'Home',
-  components: {
-  Homep
-
-  }
-}
-</script>
 

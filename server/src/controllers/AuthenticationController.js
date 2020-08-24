da862d3ce0717a398434d173c682c5b1e26effd9
Mateusz Kata <matiz.kata@gmail.com>
2020-08-24 12:45:46 +0200

    dziala logowanie przez frontend oraz rejestracja. zostaja wyslane zapytania http post

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -10,5 +10,37 @@ module.exports = {
        error: 'This email is already in use'
      })
    }
+  },
+
+  async login(req,res){
+    try {
+      const {email, password} = req.body
+      const user = await User.findOne({
+        where: {
+          email: email
+        }
+      })
+      if(!user) {
+        return res.status(403).send({
+          error: 'Incorrect login'
+        })
+      }
+
+      const isPasswordValid = password ===user.password
+      if(!isPasswordValid) {
+        return res.status(403).send({
+          error: 'Incorrect password'
+        })
+      }
+      const userJson=user.toJSON()
+      res.send({
+        user: user.toJSON()
+      })
+    }
+    catch(err) {
+      res.status(500).send({
+        error: 'Error while trying to login'
+      })
+    }
   }
 }

6f96b997c53ef96584078d3d376a43598b5bba0c
Mateusz Kata <matiz.kata@gmail.com>
2020-08-24 15:40:37 +0200

    web token dodany ale nie dziala logowanie

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,4 +1,13 @@
 const {User} = require('../models')
+const jwt = require('jsonwebtoken')
+const config = require('../config/config')
+
+function jwtSignUser (user) {
+  const ONE_WEEK = 60*60*24*7
+  return jwt.sign(user, config.authentication.jwtSecret, {
+    expiresIn: ONE_WEEK
+  })
+}
 
 module.exports = {
  async register(req, res) {
@@ -26,7 +35,7 @@ module.exports = {
         })
       }
 
-      const isPasswordValid = password ===user.password
+      const isPasswordValid = await user.comparePassword(password)
       if(!isPasswordValid) {
         return res.status(403).send({
           error: 'Incorrect password'
@@ -34,7 +43,8 @@ module.exports = {
       }
       const userJson=user.toJSON()
       res.send({
-        user: user.toJSON()
+        user: userJson(),
+        token: jwtSignUser(userJson)
       })
     }
     catch(err) {
